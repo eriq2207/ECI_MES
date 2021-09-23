b33db392fd4dba52ad4ac74658b9f529b898015b
eriq2207 <eriq2207@gmail.com>
2021-09-23 14:44:52 +0200

    fix

--- src/routers/main.ts
+++ src/routers/main.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { machineDb } from "src/controllers/machineDb";
+import { machineDb } from "../controllers/machineDb";
 import * as machine from "../models/MachineData"
 
 let router = Router()
