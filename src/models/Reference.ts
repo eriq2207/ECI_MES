2c189fd5754177977a0bbe653ec57eef756f4f16
eriq2207 <eriq2207@gmail.com>
2021-09-29 13:24:36 +0200

    last changed and config

--- src/models/Reference.ts
+++ src/models/Reference.ts
@@ -1,7 +1,7 @@
 export class Reference {
     public name: String = ""
     public project: String = ""
-    public description: String = "Opis referencji"
+    public description: String = ""
     public done : Boolean = false;
     public fromTime: Date = new Date
     public toTime: Date = new Date
